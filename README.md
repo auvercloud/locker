197b8cca6040b88b3808f6dbbeec21a81c2d9aeb
basicsio <io@auvercloud.fr>
2015-12-07 14:03:58 +0100

    version 4.0
    Brand new version

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-AuverCloud SafeNote
+AuverCloud SafeNote version 4
 ===
 
 AuverCloud SafeNote is a simple secured notes web application based on AuverCloud API server. 
 It demonstrates how to build a complete application just using client technologies (HTML, JS, CSS) and the AuverCloud API.
-The current version is based on AuverCloud API server and runtime client version 3. 
-AuverCloud SafeNote was formely known as AuverCloud Locker. 
+The current version is based on AuverCloud API server and runtime client version 3.  
 
 ##Live demo##
 The application itself is up and running at this address [https://www.auvercloud.com/app/safenote/](https://www.auvercloud.com/app/safenote/).
---
+++ js/local.4.js
@@ -0,0 +1,319 @@
+/* =================================================================================================
+ * AUVERCLOUD SAFENOTE 4.0 MESSAGES
+ * -------------------------------------------------------------------------------------------------
+ * PLEASE NOTE: To use with arc.localizeByID (lib4.js)
+ * ================================================================================================= */
+var MSG = {
+	meta_definition : {
+		en : "Secure notes manager.",
+		fr : "Gestionnaire de notes sécurisées.",
+		prop : "meta-definition"
+	},
+	ok : {
+		en : "OK",
+		fr : "OK",
+		prop : "class-html"
+	},
+	cancel : {
+		en : "cancel",
+		fr : "annuler",
+		prop : "class-html"
+	},
+	print : {
+		en : "print",
+		fr : "imprimer",
+		prop : "class-html"
+	},
+	confirm : {
+		en : "confirm",
+		fr : "confirmer",
+		prop : "class-html"
+	},
+	setup : {
+		en : "Ongoing setup.",
+		fr : "Configuration en cours.",
+		prop : "html"
+	},
+	btn_intro : {
+		en : "Skip the introduction",
+		fr : "Passer l'introduction",
+		prop : "html"
+	},
+	show_intro : {
+		en : " Play animation at startup.",
+		fr : " Jouer l'animation au démarrage.",
+		prop : "html"
+	},
+	legend_signin : {
+		en : "Sign in to your account",
+		fr : "Connexion à votre compte",
+		prop : "html"
+	},
+	label_signin_email : {
+		en : "Email address",
+		fr : "Adresse email",
+		prop : "html"
+	},
+	label_signin_pwd : {
+		en : "Password",
+		fr : "Mot de passe",
+		prop : "html"
+	},
+	label_version : {
+		en : "Version ",
+		fr : "Version ",
+		prop : "html"
+	},
+
+	legend_signup : {
+		en : "Subscription / New password",
+		fr : "Inscription / Nouveau mot de passe",
+		prop : "html"
+	},
+	label_signup_email : {
+		en : "Valid email address",
+		fr : "Adresse email valide",
+		prop : "html"
+	},
+	label_signup_challenge : {
+		en : "Result or <span class='icon-loop3 smaller'></span> to change the operation",
+		fr : "Résultat ou <span class='icon-loop3 smaller'></span> pour changer d'opération",
+		prop : "html"
+	},
+	pref_title : {
+		en : "Preferences",
+		fr : "Préférences",
+		prop : "html"
+	},
+	faq_title : {
+		en : "FAQ",
+		fr : "FAQ",
+		prop : "html"
+	},
+	btn_form_signup : {
+		en : "Subscription / Forgotten password",
+		fr : "Inscription / Mot de passe oublié",
+		prop : "html"
+	},
+	btn_form_signin : {
+		en : "Sign in to your account",
+		fr : "Connexion à votre compte",
+		prop : "html"
+	},
+	btn_signup : {
+		en : "Create<br>account",
+		fr : "Créer<br>un compte",
+		prop : "html"
+	},
+	btn_reset : {
+		en : "Reset<br>password",
+		fr : "Réinitialiser<br>mot de passe",
+		prop : "html"
+	},
+	txt_signout : {
+		en : "Sign out",
+		fr : "Se déconnecter",
+		prop : "html"
+	},
+	txt_print : {
+		en : "Print all notes",
+		fr : "Imprimer toutes les notes",
+		prop : "html"
+	},
+	txt_pref : {
+		en : "Preferences",
+		fr : "Préférences",
+		prop : "html"
+	},
+	txt_faq : {
+		en : "FAQ",
+		fr : "FAQ",
+		prop : "html"
+	},
+	txt_designed : {
+		en : "Designed by",
+		fr : "Conçu par",
+		prop : "html"
+	},
+	discover_menu : {
+		en : "Click here to display the menu of additional services: sign out, printing, preferences and FAQ.",
+		fr : "Cliquez ici pour faire apparaître le menu de services supplémentaires&nbsp;: déconnexion, impression, préférences et FAQ.",
+		prop : "html"
+	},
+	discover_plus : {
+		en : "Click here to create your first <q class='blue'>SafeNote</q>.<br> Then, the very first time you change a note, you will have to define the key to encrypt your notes.",
+		fr : "Cliquez ici pour créer votre première <q class='blue'>SafeNote</q>.<br>Ensuite, la toute première fois que vous modifierez une note, vous devrez définir la clé qui sera utilisée pour le chiffrement de vos notes.",
+		prop : "html"
+	},
+	label_name : {
+		en : "Note name",
+		fr : "Nom de la note",
+		prop : "html"
+	},
+	label_content : {
+		en : "Secured content",
+		fr : "Contenu sécurisé",
+		prop : "html"
+	},
+	legend_password : {
+		en : "New password",
+		fr : "Nouveau mot de passe",
+		prop : "html"
+	},
+	label_password1 : {
+		en : "8-16 digits password",
+		fr : "Mot de passe de 8 à 16 chiffres",
+		prop : "html"
+	},
+	label_password2 : {
+		en : "Same as above",
+		fr : "Le même que ci-dessus",
+		prop : "html"
+	},
+	legend_unsubscribe : {
+		en : "Unsubscribe",
+		fr : "Désinscription",
+		prop : "html"
+	},
+	key_new_legend : {
+		en : "Define your cyphering key",
+		fr : "Définissez votre clé de chiffrement",
+		prop : "html"
+	},
+	key_new_label_1 : {
+		en : "You cyphering key must be defined with <b>at least 8 alphanumeric characters</b>.<br>Pay attention to its definition! Unlike your password, <b>your cyphering key cannot be changed</b> and is frequently required.",
+		fr : "Votre clé de chiffrement doit être composée <b>d'au moins 8 caractères alphanumériques</b>.<br>Définissez la avec précaution&nbsp;! Contrairement au mot de passe, <b>votre clé ne pourra pas être modifiée</b> et vous sera demandée régulièrement.",
+		prop : "html"
+	},
+	key_new_label_2 : {
+		en : "Same as above",
+		fr : "Le même que ci-dessus",
+		prop : "html"
+	},
+	key_enter_legend : {
+		en : "Please enter your cyphering key",
+		fr : "Saisissez votre clé de chiffrement SVP",
+		prop : "html"
+	},
+	label_unsubscribe : {
+		en : "To unsubscribe from the SafeNote service, please send an email to <b class='blue'>safenote@auvercloud.com</b> from the subscribed email address, mentioning the keyword UNSUBSCRIBE in the subject.",
+		fr : "Pour vous désinscrire du service SafeNote, veuillez envoyer un email à <b class='blue'>safenote@auvercloud.com</b> à partir de votre adresse d'inscription en mentionnant le mot-clé UNSUBSCRIBE dans le sujet du message.",
+		prop : "html"
+	},
+	tip_signin_email : {
+		en : "It is required to sign up before to sign in.",
+		fr : "Il est nécessaire de s'inscrire avant de se connecter.",
+		prop : "title"
+	},
+	tip_signin_pwd : {
+		en : "In case of repeated connection errors, the account is locked and then the password must be reset.",
+		fr : "En cas d'erreurs de connexions répétées, le compte se bloque et le mot de passe doit alors être réinitialisé.",
+		prop : "title"
+	},
+	tip_signup_email : {
+		en : "This address will receive the new password on subscription or account reset.",
+		fr : "C'est cette adresse qui recevra le nouveau mot de passe lors de l'inscription ou à la réinitialisation de compte.",
+		prop : "title"
+	},
+	tip_note_name : {
+		en : "No more than 80 characters.",
+		fr : "Pas plus de 80 caractères.",
+		prop : "title"
+	},
+	tip_note_content : {
+		en : "No more than 4000 characters.",
+		fr : "Pas plus de 4000 caractères.",
+		prop : "title"
+	},
+	err606 : {
+		en : "Not allowed value(s).",
+		fr : "Valeur(s) non acceptée(s)."
+	},
+	err641 : {
+		en : "Invalid token or operation already performed.",
+		fr : "Jeton non valide ou opération déjà réalisée. ",
+	},
+	err643 : {
+		en : "The operation result is wrong.",
+		fr : "Le résultat de l'opération est faux."
+	},
+	err650 : {
+		en : "The email address is already used.",
+		fr : "L'adresse email est déjà utilisée."
+	},
+	err651 : {
+		en : "The email address is unknowm.",
+		fr : "L'adresse email inconnue."
+	},
+	err652 : {
+		en : "Wrong password. You have %number% attempts left before locking you account.",
+		fr : "Mauvais mot de passe. Il vous reste %number% tentatives avant de bloquer votre compte."
+	},
+	err653 : {
+		en : "Your account is locked. Please reset your password.",
+		fr : "Votre compte est bloqué. Veuillez réinitialiser votre mot de passe."
+	},
+	err662 : {
+		en : "Invalid email address(es).",
+		fr : "Adresse(s) email non valide(s)."
+	},
+	err663 : {
+		en : "Empty recipient list.",
+		fr : "Liste de destinataires vide."
+	},
+	err_pwd_invalid : {
+		en : "Invalid password.",
+		fr : "Mot de passe invalide."
+	},
+	err_pwd_match : {
+		en : "The inputs do not match.",
+		fr : "Les entrées ne correspondent pas."
+	},
+	subject_signup : {
+		en : "Please confirm your subscription to SafeNote",
+		fr : "Veuillez confirmer votre inscription à SafeNote"
+	},
+	subject_reset : {
+		en : "Please confirm your password reset",
+		fr : "Veuillez confirmer la réinitialisation de votre mot de passe"
+	},
+	notif_signup : {
+		en : "Please consult the inbox of the email address ",
+		fr : "Veuillez consulter la boîte de réception de l'adresse email "
+	},
+	callback_subscribe : {
+		en : "Registration is complete for the email address ",
+		fr : "Inscription validée pour l'adresse email "
+	},
+	callback_reset : {
+		en : "Password reset is complete for the email address ",
+		fr : "Le mot de passe a été réinitialisé pour l'adresse email "
+	},
+	default_note_name : {
+		en : "New note: Click to edit!",
+		fr : "Nouvelle note : Cliquez pour éditer !"
+	},
+	delete_cancel : {
+		en : "cancel<br>delete",
+		fr : "annuler<br>effacement",
+	},
+	delete_confirm : {
+		en : "confirm<br>delete",
+		fr : "confirmer<br>effacement",
+	},
+	key_not_valid : {
+		en : "The key is not valid.",
+		fr : "La clé n'est pas valide.",
+	},
+	key_not_match : {
+		en : "The inputs do not match.",
+		fr : "Les saisies ne coïncident pas.",
+	},
+	key_wrong : {
+		en : "Wrong cyphering key.",
+		fr : "Mauvaise clé de chiffrement.",
+	}
+};
+/* =================================================================================================
+ * EoF
+ * ================================================================================================= */
